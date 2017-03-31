8295d45b8c939d269079c2a461385655946775da
techgineer <ahdavis13@gmail.com>
2017-03-31 14:59:36 -0500

    Made window bigger

--- src/util/constants.cpp
+++ src/util/constants.cpp
@@ -10,7 +10,7 @@
 #include "constants.h"
 
 //screen size constants
-const int SCREEN_WIDTH = 880; //the width of the screen
+const int SCREEN_WIDTH = 860; //the width of the screen
 const int SCREEN_HEIGHT = 700; //the height of the screen
 
 //the header text for the game window
